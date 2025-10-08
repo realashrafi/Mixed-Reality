7baa810d7b2ae522b1a6dd686297285529f547c7
ali <zboom0ne01@gmail.com>
2025-10-08 12:29:43 +0330

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import './App.css';
 import ARIntegrated from "./pages/MR/ARIntegrated";
+import ARIntegrated2 from "./pages/MR/v2/ARIntegrated2";
 
 function App() {
   return (
       <Router>
         <Routes>
           <Route path="/" element={<ARIntegrated/>}/>
+          <Route path="/1" element={<ARIntegrated2 anchorImage={'/targets.mind'}/>}/>
         </Routes>
       </Router>
   );
